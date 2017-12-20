7ace815e19d97d33ac2e669f3c9af5b8030046b8
jsburklund <robomower9508@gmail.com>
2017-12-20 14:10:52 -0500

    Change output so the planks are more uniform in length

--- boardviewterminal.py
+++ boardviewterminal.py
@@ -6,12 +6,12 @@ class BoardViewTerminal:
     for i in range(size[1]*2):
       temp_row = []
       for j in range(size[0]*2):
-        temp_row.append(' ')
+        temp_row.append('  ')
       temp.append(temp_row)
 
     #Draw the stumps first
     for stump in board.get_stumps():
-      temp[stump[1]*2][stump[0]*2] = 'x'
+      temp[stump[1]*2][stump[0]*2] = 'x '
 
     #Draw the planks
     for plank in board.get_planks():
@@ -21,26 +21,26 @@ class BoardViewTerminal:
       if plank.is_horizontal():
         #Plank is horizontal
         for i in range((length*2)+1):
-          temp[start_point[1]*2][(start_point[0]*2)+i+1] = '-'
+          temp[start_point[1]*2][(start_point[0]*2)+i+1] = '- '
       else:
         #Plank is vertical
         for i in range((length*2)+1):
-          temp[(start_point[1]*2)+i+1][start_point[0]*2] = '|'
+          temp[(start_point[1]*2)+i+1][start_point[0]*2] = '| '
 
     #Draw the player
     player_loc = board.get_player().get_loc()
-    temp[player_loc[1]*2][player_loc[0]*2] = 'P'
+    temp[player_loc[1]*2][player_loc[0]*2] = 'P '
 
     # Render everything
-    str = '--' + '-'*size[0]*2 + '-\n'
-    str += '| ' + '  '*size[0] + '|\n'
+    str = '--' + '--'*size[0]*2 + '-\n'
+    str += '| ' + '    '*size[0] + '|\n'
     for row in temp:
       str += '| '
       for col in row:
         str += col
       str += '|\n'
     # buffer row is already drawn as artifact
-    str += '--' + '-'*size[0]*2 + '-'
+    str += '--' + '--'*size[0]*2 + '-'
     print(str)
 
 if __name__ == '__main__':
