b66b9b6ade56faa9ca23878f461e17cf018643df
jsburklund <robomower9508@gmail.com>
2017-12-20 14:51:05 -0500

    Implement puzzle #2

--- boardfactory.py
+++ boardfactory.py
@@ -1,11 +1,16 @@
-import board
-import plank
+from board import Board
+from plank import Plank
 
 class BoardFactoryPack1:
   def make_board(puzzlenum):
    if puzzlenum not in range(1,40+1):
      raise ValueError('Not a valid puzzle Number')
-   if puzzlenum in range(2,40+1):
+   if puzzlenum in range(3,40+1):
      raise ValueError('Puzzle not yet implemented')
-   if puzzlenum == 1:
-     return board.Board([(1,4),(1,3),(3,3),(3,2),], (1,6), (3,0), [plank.Plank(1,(1,4),(1,3)), plank.Plank(2,(1,6),(1,4))])
+
+   puzzles = {
+     1: Board([(1,4),(1,3),(3,3),(3,2),], (1,6), (3,0), [Plank(1,(1,4),(1,3)), Plank(2,(1,6),(1,4))]),
+     2: Board([(4,1),(4,2),(2,2),(2,3),(2,4),(0,4),(0,5)], (0,6), (4,0), [Plank(2,(4,2),(2,2)), Plank(2,(0,4),(2,4)), Plank(1,(0,6),(0,5))]),
+   }
+   return puzzles[puzzlenum]
+   
